e4023d2ebc45e3b595f9ca2c461b8da598b9eb8a
martapryczkowska <marta.pryczkowska@gmail.com>
2018-05-26 21:07:36 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace rs232
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.richTextBox1.Font = new System.Drawing.Font("MS Reference Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 128);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(360, 56);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.richTextBox2.Font = new System.Drawing.Font("MS Reference Sans Serif", 16.2F);
+            this.richTextBox2.Location = new System.Drawing.Point(442, 96);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(439, 147);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.BackColor = System.Drawing.Color.Lime;
+            this.richTextBox3.Font = new System.Drawing.Font("MS Reference Sans Serif", 16.2F);
+            this.richTextBox3.Location = new System.Drawing.Point(951, 128);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(382, 56);
+            this.richTextBox3.TabIndex = 2;
+            this.richTextBox3.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Bahnschrift", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(90, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 40);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "nadajnik";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Bahnschrift", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1019, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(221, 36);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "odbiornik";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Bahnschrift", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(551, 246);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(221, 36);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "konwersja";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Bahnschrift", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(-28, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(1447, 35);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Transmisja szeregowa rs232";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkGreen;
+            this.button1.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(1134, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 44);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "transmisja";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::rs232.Properties.Resources.right;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = global::rs232.Properties.Resources.right;
+            this.pictureBox1.Location = new System.Drawing.Point(887, 128);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(58, 56);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::rs232.Properties.Resources.right;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(378, 128);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(58, 56);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1418, 310);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "rs232";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rs232
+{
+    public partial class Form1 : Form
+    {
+        string theText;
+        List<string> censoredWords = new List<string>();
+        string word;
+        string binaryText;
+        string encodedText;
+        String[] wordcen;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            readFile();
+        }
+
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            theText = richTextBox1.Text;
+        }
+
+
+        public void readFile()
+        {
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader("black_list.txt"))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    char delimiter = '\n';
+                    string line = sr.ReadToEnd();
+                     wordcen = line.Split(delimiter);
+                  //  wordCen = line.Split(delimiter);
+                   // censoredWords.Add(wordCen);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    
+
+
+        public string  censor(string word)
+        {
+            string returnedWord=word;
+               foreach (string wordcens in wordcen)
+                {
+                    Console.WriteLine(wordcens);
+                    bool compare = wordcens.Contains(word);
+                    if (compare)
+                    {
+                        Console.WriteLine("cenzura");
+                        returnedWord = new string('*', word.Length);
+                }
+            }
+            return returnedWord+" ";
+
+        }
+
+
+        public void ascii2bin(string text)
+        {
+            Char delimiter = ' ';
+            String[] substrings = text.Split(delimiter);
+            text = "";
+            foreach (string substring in substrings)
+            {
+                text+=censor(substring);
+            }
+            richTextBox1.Text = text;
+
+            foreach (char c in text) {                          // dla każdego znaku
+                binaryText = binaryText + '0';                  //bit startu
+                word=(Convert.ToString(c, 2).PadLeft(8, '0'));  //ascii2bin
+                char[] charArray = word.ToCharArray();          // bity odwrócona kolejność
+                Array.Reverse(charArray);
+                string s = new string(charArray);
+                    binaryText += s;                            //
+                    binaryText =binaryText+ '1';    //bity stopu
+                binaryText = binaryText + '1';       //tekst wyjściowy
+
+            }
+
+        }
+
+
+        public void receive(string binText)
+        {
+            string word2;
+            encodedText="";
+
+            int length = (binaryText.Length/11);           //ilość znaków
+            for (int i=0; i<length; i++){                   
+                word2 = binaryText.Substring((11*i)+1, 8);      //znak z pominięciem bitu startu i stopu
+                char[] charArray = word2.ToCharArray();         
+                Array.Reverse(charArray);
+                string s = new string(charArray);
+                var number = Convert.ToInt32(s, 2);        //konwersja do ASCII
+                encodedText += (char)number;
+
+
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ascii2bin(theText);
+            richTextBox2.Text = binaryText;
+            receive(binaryText);              
+            richTextBox3.Text =encodedText;
+            binaryText = "";
+            encodedText = "";
+
+        }
+
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
